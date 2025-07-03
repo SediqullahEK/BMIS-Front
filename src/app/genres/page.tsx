f2d81633d26441dbc46594b89e8b4514778724ae
Sediqullah Ekhtyar <a12343358@unet.univie.ac.at>
2025-07-03 23:54:36 +0200

    C

--- src/app/genres/page.tsx
+++ src/app/genres/page.tsx
@@ -1,9 +1,9 @@
-export default function Books() {
+export default function Genres() {
    
     return (
         <div className="flex flex-col items-center justify-top h-screen">
             <h1 className="text-4xl font-bold mb-4 text-slate-50 pt-8">
-                Books List Page
+                Genres List Page
             </h1>
         </div>
     );
